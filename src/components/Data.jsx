e8f7b068c1b551d77fb8f5c0de972992edb57807
Beka <past.present.future.end@gmail.com>
2023-02-27 20:07:22 +0400

    in

---
+++ src/components/Data.jsx
@@ -0,0 +1,49 @@
+import redPanda from '../src-carusel/Red-Panda.jpg'
+import flyingFox from '../src-carusel/Flying-fox.webp'
+import pangolin from '../src-carusel/Pangolin.webp'
+import monkey from '../src-carusel/Proboscis-monkey.webp'
+import eagle from '../src-carusel/Philippine-Eagle.webp'
+
+
+const reviews = [
+  {
+    id: 1,
+    name: "Red panda",
+    type: 'Preditor',
+    image: redPanda,
+    text: "You might know this one as the star of the movie Turning Red. The red panda is an adorable mammal, native to China and the Himalayas. With red and brown fur and cat-like ears, it looks more like a raccoon than a panda.",
+  },
+  {
+    id: 2,
+    name: "Flying fox",
+    type: 'Preditor',
+    image: flyingFox,
+    text:
+    "This animal isn’t actually a fox that flies. But it’s easy to understand how it got that name. A flying fox is a type of bat, called a “megabat,” because of its huge size. But don't let that scare you. They mostly eat fruit, which is why they're also known",
+  },
+  {
+    id: 3,
+    name: 'Pangolin',
+    type: 'Preditor',
+    image: pangolin ,
+    text:
+    "The pangolin is a unique mammal found in Asia and Africa. It's known for its overlapping scales that look a bit like medieval armour. When threatened, the pangolin curls up into an armoured ball and looks like a giant pine cone",
+  },
+  {
+    id: 4,
+    name: "Proboscis monkey",
+    type: 'Preditor',
+    image: monkey,
+    text: "This might be one of the silliest-looking monkeys in the world! The proboscis  monkey can be identified by its long nose and bright coat. Proboscis actually means nose, and is how the animal got its name. The males tend to have large, bulbous noses. ",
+  },
+  {
+    id: 5,
+    name: "Eagle",
+    type: 'Preditor',
+    image: eagle,
+    text: "It's the biggest eagle in the world in terms of wingspan. From end to end, their average wingspan is around two metres! That's as big as a cougar! At that size, it’s no surprise they can hunt anything from monkeys to flying foxes",
+  },
+
+];
+
+export default reviews;
